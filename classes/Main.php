7ece479c204dea1826d88c23cf83eca78127fe94
VovchenkoMykyta <vovchenkonikita36@gmail.com>
2021-05-16 22:14:56 +0300

    add static function to Main.php

--- classes/Main.php
+++ classes/Main.php
@@ -4,6 +4,9 @@ include_once 'Storage.php';
 
 class Main
 {
+    /**
+     * Inititialization function
+     */
     static function init()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'GET' && empty($_GET['id'])) {
@@ -13,8 +16,24 @@ class Main
             file_put_contents(Storage::$storageFile, $_POST['note']);
             fclose($file);
             header("Location: index.php");
+        }elseif (!is_int($_GET['id'])){
+            self::error404();
         } elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && !empty($_GET['id'])){
-
+            $newFile = fopen(Storage::$storageFile, 'r');
+            file_get_contents(Storage::$storageFile, false,null,$_GET['id']);
+            fclose($newFile);
         }
+    }
+
+    /** Show error 404
+     * @return string
+     */
+    static function error404(){
+        return 'There is no page, fatality';
+    }
+
+    static function redirect(){
+        header('Location: ./');
+        exit();
     }
 }
