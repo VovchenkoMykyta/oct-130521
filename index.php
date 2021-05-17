48805756f00ec9e8a3a4363d71f987a2d9762580
VovchenkoMykyta <vovchenkonikita36@gmail.com>
2021-05-17 23:35:27 +0300

    preg commit

--- index.php
+++ index.php
@@ -8,22 +8,13 @@ spl_autoload_register(function ($className){
        return false;
    }
 });
-$id = 0;
-
 Main::init();
 $storage = new Storage("data/data.txt");
-var_dump($storage->getAllNotes());
-
-var_dump($_POST);
 ?>
 
 <form method="post">
     <textarea name="note" placeholder="Enter note"></textarea>
-    <input type="hidden" name="id" value="<?= ++$id ?>">
     <input type="submit" name="submit">
 </form>
-<?php
-$notes[] = $storage->getAllNotes();
-var_dump($notes);
-?>
+<?php echo $storage->getAllNotes();?>
 
